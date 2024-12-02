e3f5040fda2a75ea9f16c2501b1af346b67346ba
dnomjr <dano.majer@protonmail.com>
2024-12-01 21:12:12 -0500

    second project

--- eat-n-split/src/App.js
+++ eat-n-split/src/App.js
@@ -1,25 +1,106 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
+const initialFriends = [
+  {
+    id: 118836,
+    name: 'Clark',
+    image: 'https://i.pravatar.cc/48?u=118836',
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: 'Sarah',
+    image: 'https://i.pravatar.cc/48?u=933372',
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: 'Anthony',
+    image: 'https://i.pravatar.cc/48?u=499476',
+    balance: 0,
+  },
+]
+
+function Button({ text, onClick }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <button className='button' onClick={onClick}>
+      {text}
+    </button>
+  )
 }
 
-export default App;
+function App() {
+  const [friends, setFriends] = useState(initialFriends)
+
+  return (
+    <div className='app'>
+      <Sidebar friends={friends} />
+      <SplitBill friends={friends} />
+    </div>
+  )
+}
+
+function Sidebar({ friends }) {
+  const [toggle, setToggle] = useState(false)
+
+  function showAdd() {
+    setToggle(toggle => !toggle)
+  }
+  return (
+    <div className='sidebar'>
+      <ul>
+        {friends.map(friend => {
+          return <Friend key={friend.id} friend={friend} />
+        })}
+      </ul>
+
+      {toggle && (
+        <form action='form-add-friend'>
+          <label>👫 Friend name</label>
+          <input type='text' />
+          <label>🌄 Image URL</label>
+          <input type='text' defaultValue='https://i.pravatar.cc/48' />
+          <Button text='Add' />
+        </form>
+      )}
+
+      <Button text={toggle ? 'Close' : 'Add friend'} onClick={showAdd} />
+    </div>
+  )
+}
+
+function Friend({ friend }) {
+  const { name, balance, image } = friend
+
+  return (
+    <li>
+      <img src={image} alt='Person' />
+      <div>
+        <h3>{name}</h3>
+        <p className={balance < 0 ? 'red' : balance > 0 ? 'green' : ''}>
+          {name} owes you {balance}€
+        </p>
+      </div>
+      <Button text='Select' />
+    </li>
+  )
+}
+
+function SplitBill({}) {
+  return (
+    <form className='form-split-bill'>
+      <h2>Split a bill with Anthony</h2>
+      <label>💰 Bill value </label>
+      <input type='text' />
+      <label>🧍‍♀️ Your expense</label>
+      <input type='text' />
+      <label>👫 's expense</label>
+      <input type='text' disabled />
+      <label>🤑 Who is paying the bill</label>
+      <input type='text' />
+      <Button text='Split bill' />
+    </form>
+  )
+}
+
+export default App
